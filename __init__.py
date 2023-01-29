0b22623c50741cda566efced703151ef0a082c1d
Wang David <wangdavid1999@gmail.com>
2023-01-29 15:39:25 +0100

    Add Swagger for API contract

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ Init module
 """
 from flask import Flask
 from flask_cors import CORS
+from flasgger import Swagger
 from .db import db
 from .routes import main
 
@@ -11,8 +12,10 @@ def create_app():
     Init application with:
     Flask
     CORS
+    Swagger
     """
     app = Flask(__name__)
+    Swagger(app)
     CORS(app)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/extract-pdf-test" #Database configuration URI
